4b5722e48ab6f8529026359ec3fc9d6c48877e23
Daniel Weber <d.weber16@zohomail.com>
2022-07-10 22:43:57 -0500

    completed code file

---
+++ README.md
@@ -0,0 +1,7 @@
+Code for slot machine problem in Learn To Code By Solving Problems book. 
+
+
+Three slot machines being played in turn. Each machine pays out predictably. Code returns how many times you can play total before running out of quarters.
+
+First input is number of starting quarters.
+Second, third, and fourth inputs are the number of plays since each respective machine has payed out.
--- slot_machine.py
+++ slot_machine.py
@@ -1,6 +1,36 @@
-starting_quarters = int(input())
-#number of times since last payout
-mach_one_played = int(input()) 
-mach_two_played = int(input())
-mach_three_played = int(input())
+#number of starting quarters
+quarters = int(input())
+#number of times machines have been played since last payout
+mach_one = int(input())
+mach_two = int(input())
+mach_three = int(input())
+#machine to be played next
+next_machine = 0
+#total number of times played
+total_plays = 0
 
+
+while quarters >= 1:
+    quarters -= 1
+    
+    if next_machine == 0:
+        mach_one += 1
+        if mach_one == 35:
+            mach_one = 0
+            quarters += 30
+    elif next_machine == 1:
+        mach_two += 1
+        if mach_two == 100:
+            mach_two = 0
+            quarters += 60
+    elif next_machine == 2:
+        mach_three += 1
+        if mach_three == 10:
+            mach_three = 0
+            quarters += 9
+    total_plays += 1
+    next_machine += 1
+    if next_machine == 3:
+        next_machine = 0
+
+print(f"Martha plays {total_plays} times before going broke.")
